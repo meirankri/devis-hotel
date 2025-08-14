aefd9c5e295af3f1168979c57b99abc83f59402d
meirankri <meirankri@gmail.com>
2025-08-14 08:58:30 +0200

    connexion only with google

--- components/SignForm.tsx
+++ components/SignForm.tsx
@@ -177,7 +177,7 @@ export function SignForm() {
         </div>
       )}
 
-      <div className="mb-8 flex items-center justify-center">
+      {/* <div className="mb-8 flex items-center justify-center">
         <span className="hidden h-[1px] w-full max-w-[70px] bg-body-color/50 sm:block"></span>
         <p className="w-full px-5 text-center text-base font-medium text-body-color">
           {t("orSignInWithEmail")}
@@ -213,7 +213,7 @@ export function SignForm() {
             </Button>
           </div>
         </form>
-      </Form>
+      </Form> */}
     </>
   );
 }
